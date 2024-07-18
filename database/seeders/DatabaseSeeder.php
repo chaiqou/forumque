172e5d78cd16c5816c23613ffe7da9306033be9d
Nikoloz Lomtadze <84349052+chaiqou@users.noreply.github.com>
2024-07-18 18:52:59 +0400

    feat: use recycle method for db seed to use already created relationships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $users = User::factory(10)->create();;
+        $posts = Post::factory(50)->recycle($users)->create();
+        $comments = Comment::factory(100)->recycle($users)->recycle($posts)->create();
     }
 }
